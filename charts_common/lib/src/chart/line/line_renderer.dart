a1d7cd82c92058d6ad2bdb2c7fd6dd05a78affcd
Mehmet Şeref Kayalı <mskayali@yandex.com>
2020-01-04 09:52:40 +0300

    Update line_renderer.dart

--- charts_common/lib/src/chart/line/line_renderer.dart
+++ charts_common/lib/src/chart/line/line_renderer.dart
@@ -984,6 +984,7 @@ class LineRenderer<D> extends BaseCartesianRenderer<D> {
                 points: line.points,
                 stroke: line.color,
                 strokeWidthPx: line.strokeWidthPx,
+                smoothLine: config.smoothLine,
                 roundEndCaps: line.roundEndCaps);
           }
         });
